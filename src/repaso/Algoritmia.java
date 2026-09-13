5ae17fc76e4ce07724d51a1652401b0f0bc5052a
brfer <brfer@Natalia>
2026-09-13 11:47:19 +0200

    Commit

---
+++ src/repaso/Algoritmia.java
@@ -0,0 +1,14 @@
+package repaso;
+
+import java.util.Scanner;
+
+public class Algoritmia {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un entero:");
+		int x = sc.nextInt();
+		System.out.println("Número es: " + x);
+
+	}
+}
---
+++ src/repaso/EjerciciosTema1.java
@@ -0,0 +1,10 @@
+package repaso;
+
+public class EjerciciosTema1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
